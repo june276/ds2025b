46f1540e11dcb0ab1182adb347491a7e564343fd
june276 <jungjune276@gmail.com>
2025-04-07 11:11:54 +0900

    week5 stack 1.0

---
+++ week05.py
@@ -0,0 +1,23 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        return self.items[-1]
+
+s1 = Stack()
+print(s1.is_empty())
+s1.push("자료구조")
+print(s1.is_empty())
