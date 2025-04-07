cdbc2406390c7011e4883f2ca2caeedde88119f6
june276 <jungjune276@gmail.com>
2025-04-07 11:44:21 +0900

    week5 stack 2.1 list ver.

--- week05.py
+++ week05.py
@@ -1,30 +1,10 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.link = None
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty"
-        popped_node = self.top
-        self.top = self.top.link
-        return popped_node.data
-
-s1 = Stack()
+s1 = list()
+s1.append("data structure") # push
+s1.append("database") # push
+print(len(s1))  # size
+print(s1[-1])   # peek
+print(s1)
 print(s1.pop())
-s1.push("data structure")
-s1.push("database")
+print(s1)
 print(s1.pop())
-print(s1.pop())
+print(s1)
