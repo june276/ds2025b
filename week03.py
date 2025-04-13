ff0b08cbb9af342ba340808b2a83d17ab2cbde6c
june276 <jungjune276@gmail.com>
2025-04-13 15:33:39 +0900

    week3 arr zero move

--- week03.py
+++ week03.py
@@ -12,3 +12,18 @@ def duplicate(city):
 
 cities = ['Incheon', "Incheon", "Paju", "Incheon", "Seoul", "Gimpo", "Seoul"]
 
+def move_zero(a_list):
+    zero_index = 0
+    for index, n in enumerate(a_list):
+        print(f"zi={zero_index} index={index}\tbefore : ",a_list, end=" ")
+        if n != 0:
+            a_list[zero_index] = n
+            if zero_index != index:
+                a_list[index] = 0
+            zero_index += 1
+        print(f"\tafter : ", a_list)
+    return(a_list)
+
+
+
+print(move_zero([8,0,3,9,0,0,2,4, 0,5,6,]))
